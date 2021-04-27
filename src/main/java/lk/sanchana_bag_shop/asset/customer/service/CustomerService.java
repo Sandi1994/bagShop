96c1a9463f1f054f03b8efc06b095cb705ae247b
Sandi1994 <sadanimalsh@gmail.com>
2021-04-28 02:29:05 +0530

    user, supplier ui changed, mail and mobile no service code added,

--- src/main/java/lk/sanchana_bag_shop/asset/customer/service/CustomerService.java
+++ src/main/java/lk/sanchana_bag_shop/asset/customer/service/CustomerService.java
@@ -5,6 +5,8 @@ import lk.sanchana_bag_shop.asset.common_asset.model.enums.LiveDead;
 import lk.sanchana_bag_shop.asset.customer.dao.CustomerDao;
 import lk.sanchana_bag_shop.asset.customer.entity.Customer;
 import lk.sanchana_bag_shop.util.interfaces.AbstractService;
+import lk.sanchana_bag_shop.util.service.EmailService;
+import lk.sanchana_bag_shop.util.service.TwilioMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.domain.Example;
@@ -18,10 +20,14 @@ import java.util.stream.Collectors;
 @CacheConfig( cacheNames = "customer" )
 public class CustomerService implements AbstractService<Customer, Integer> {
     private final CustomerDao customerDao;
+    private final EmailService emailService;
+    private final TwilioMessageService twilioMessageService;
 
     @Autowired
-    public CustomerService(CustomerDao customerDao) {
+    public CustomerService(CustomerDao customerDao, EmailService emailService, TwilioMessageService twilioMessageService) {
         this.customerDao = customerDao;
+        this.emailService = emailService;
+        this.twilioMessageService = twilioMessageService;
     }
 
     public List<Customer> findAll() {
@@ -35,10 +41,32 @@ public class CustomerService implements AbstractService<Customer, Integer> {
     }
 
     public Customer persist(Customer customer) {
+        //email service
+        Customer customer1 = customerDao.save(customer);
+        if(customer1.getEmail()!=null){
+            String message = "Dear "+customer.getName()+
+                    "\n Code    :"+customer.getCode()+
+                    "\n Customer name   :"+customer.getName()+
+                    "\n Mobile  :"+customer.getMobile()+
+                    "\n Address :"+customer.getAddress()+
+                    "\n NIC     :"+customer.getNic();
+            emailService.sendEmail(customer1.getEmail(), "Welcome to Sanchan Bag Shop", message);
+        }
+//        mobile service
+        if (customer.getMobile() != null) {
+            try {
+                String mobileNumber = customer.getMobile().substring(1, 10);
+                twilioMessageService.sendSMS("+94" + mobileNumber, "Successfully registered in " +
+                        "Sanchana Bag Shop \nPlease Check Your Email Form Further Details");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         if ( customer.getId() == null ) {
             customer.setLiveDead(LiveDead.ACTIVE);
         }
-        return customerDao.save(customer);
+        return customer1;
     }
 
     public boolean delete(Integer id) {
